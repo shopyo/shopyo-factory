75e10110ebbdc0a1812f3f76c579cd491c4145e3
Shamsuddin Rehmani <rehmani@usc.edu>
2021-06-02 22:00:34 -0400

    Update: setup.py to point to the latest shopyo [optional body]
    [optional footer]

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -2,5 +2,5 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(install_requires=["shopyo @ git+https://github.com/shopyo/shopyo@fix-imports#egg=shopyo"])
+    setup(install_requires=["shopyo @ git+https://github.com/shopyo/shopyo@dev#egg=shopyo"])
     #setup(install_requires=["shopyo==4.1.2"])
